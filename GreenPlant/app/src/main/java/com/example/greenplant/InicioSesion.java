68459136db22a061131b14c93c0297f331b8f979
angel-js <angelserrano143@gmail.com>
2022-12-12 18:10:10 -0300

    Añadiendo login completado 1/2

--- GreenPlant/app/src/main/java/com/example/greenplant/InicioSesion.java
+++ GreenPlant/app/src/main/java/com/example/greenplant/InicioSesion.java
@@ -1,18 +1,29 @@
 package com.example.greenplant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class InicioSesion extends AppCompatActivity {
 
     private EditText correo, password;
+
+    // FB
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,9 @@ public class InicioSesion extends AppCompatActivity {
 
         correo = findViewById(R.id.etCorreo);
         password = findViewById(R.id.etPasswordRegistro);
+
+        //FB
+        mAuth = FirebaseAuth.getInstance();
     }
 
     //menu
@@ -45,6 +59,37 @@ public class InicioSesion extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+    //FB
+    public void userLogin(View v){
+        String mailL = correo.getText().toString().trim();
+        String passwordL = password.getText().toString().trim();
+
+        if (mailL.isEmpty() || passwordL.isEmpty()){
+            Toast.makeText(this, "Favor ingresar los datos de Ingreso", Toast.LENGTH_SHORT).show();
+        } else {
+            loginUser(mailL, passwordL);
+        }
+    }
+
+    private void loginUser(String mailL, String passwordL) {
+        mAuth.signInWithEmailAndPassword(mailL, passwordL).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    finish();
+                    startActivity(new Intent(InicioSesion.this, menuSeleccion.class));
+                    Toast.makeText(InicioSesion.this, "Bienvenido a su sesión!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(InicioSesion.this, "Error!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(InicioSesion.this, "Error al Iniciar Sesión", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
